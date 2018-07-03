a15f2960aebd36347e131fb2ceb853d03ff9d93c
Jaime Hernández <jaimevazz@gmail.com>
2018-07-03 16:11:36 -0500

    Project uploaded
    First code class and Settings

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour 
+{
+	private Player player;
+
+	// Use this for initialization
+	void Start () 
+	{
+		player = new Player();
+		player.Damage();
+		print("Hello World!");
+		print(player.Health);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿// Jaime 
+// 2018
+
+public class Player
+{
+	bool isDead = false;
+	private int numItems = 400;
+	// private long numBactery = 0;
+	private float health = 1.0f;
+	// private double mana = 0.0;
+	private new string name = "Player";
+	private string clanName = "Gryffindor";
+
+	// Propierties
+	public float Health
+	{
+		get {return health;}
+		set {health = value;}
+	}
+
+	public void Damage()
+	{
+		health = health - 0.1f;
+	}
+}
