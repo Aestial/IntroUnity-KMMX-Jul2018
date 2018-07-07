ae39588872755fb836badf971e74e305d66c31be
Jaime <jaimevazz@gmail.com>
2018-07-06 19:47:37 -0500

    Bullet Instance

---
+++ Assets/SpaceInvaders/Scripts/BulletMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    public float speed = 10;
+
+    private Transform transform;
+
+	void Start () 
+    {
+        transform = GetComponent<Transform>();
+	}
+	
+	void Update () 
+    {
+        float y = Time.deltaTime * speed;
+        transform.position += new Vector3(0.0f, y);
+	}
+}
---
+++ Assets/SpaceInvaders/Scripts/PlayerShoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour 
+{
+    public GameObject prefab;
+
+    Transform transform;
+    Vector3 position;
+
+	void Start () 
+    {
+        transform = GetComponent<Transform>();
+
+	}
+	
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            position = transform.position;
+            Debug.Log("Player Shoot!!!");
+            Instantiate(prefab, position, Quaternion.identity);
+        }
+	}
+}
