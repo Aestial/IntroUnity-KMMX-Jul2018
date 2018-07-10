6d98eecd2044b0d8f9b51604d012fbe8375848ad
Jaime Hernandez <jaimevazz@gmail.com>
2018-07-09 21:03:41 -0500

    Clase 06. GameManager and Canvas elements

---
+++ Assets/Jaime/SpaceInvaders/Scripts/GameManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum GameState
+{
+    Begin = 0,
+    Playing = 1,
+    End = 2,
+}
+
+public class GameManager : MonoBehaviour
+{
+    // SINGLETON
+    public static GameManager instance;
+
+    public Transform enemy;
+    public Transform player;
+    public Canvas mainMenu;
+
+    private GameState current;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        SetGameState(GameState.Begin);
+    }
+
+    public void StartGame()
+    {
+        SetGameState(GameState.Playing);
+    }
+
+    public void GameOver()
+    {
+        SetGameState(GameState.End);
+    }
+
+    public void BackToMenu()
+    {
+        SetGameState(GameState.Begin);
+    }
+
+    void SetGameState(GameState newGameState)
+    {
+
+        if (newGameState == GameState.Begin)
+        {
+            Debug.Log("Game Start");
+            mainMenu.enabled = true;
+            enemy.gameObject.SetActive(false);
+            player.gameObject.SetActive(false);
+
+        }  
+
+        else if (newGameState == GameState.Playing)
+        {
+            Debug.Log("Playing");
+            mainMenu.enabled = false;
+            enemy.gameObject.SetActive(true);
+            player.gameObject.SetActive(true);
+
+        }
+
+        else if (newGameState == GameState.End)
+        {
+            Debug.Log("Game Over");
+            mainMenu.enabled = false;
+            enemy.gameObject.SetActive(false);
+            player.gameObject.SetActive(false);
+
+        }
+
+        this.current = newGameState;
+    }
+
+}
