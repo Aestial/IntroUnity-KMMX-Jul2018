18ce16a48cebd1bd5c1d90f6a6b9acf59a2317ee
Jaime Hernandez <jaimevazz@gmail.com>
2018-07-14 22:13:29 -0500

    Cow game

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class LevelManager : MonoBehaviour 
+{
+    [SerializeField] private GameObject floorPrefab;
+    [SerializeField] private int floorLength;
+    //[SerializeField] private float 
+
+
+	void Update () 
+    {
+        DeleteCurrentFloor();
+        SpriteRenderer sp = floorPrefab.GetComponent<SpriteRenderer>();
+        float width = sp.size.x * floorPrefab.transform.localScale.x;
+        Debug.Log(width);
+        for (int i = 0; i < floorLength; i++)
+        {
+            Vector2 position = new Vector2(i * width, 0.0f);
+            GameObject newFloor = Instantiate(floorPrefab, position, Quaternion.identity, this.transform);
+        }
+	}
+
+    private void DeleteCurrentFloor()
+    {
+        int numChildren = this.transform.childCount;
+        for (int i = 0; i < numChildren; i++)
+        {
+            DestroyImmediate(this.transform.GetChild(0).gameObject);
+        }
+    }
+	
+	//void Update () 
+ //   {
+		
+	//}
+}
